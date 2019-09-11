ccb69af94ffd9dc4c2ea98924e703f0bc30fe295
Dave <DaveBathnes@users.noreply.github.com>
2019-09-12 00:12:41 +0100

    Updates

---
+++ precache-manifest.40ae7afc0d71caa129ee4c2d7752be40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5245595453a663ec4f3b0f3607396faf",
+    "url": "/RoadTraffic/index.html"
+  },
+  {
+    "revision": "78938a67bcb31958a183",
+    "url": "/RoadTraffic/static/css/2.e71d8db5.chunk.css"
+  },
+  {
+    "revision": "78938a67bcb31958a183",
+    "url": "/RoadTraffic/static/js/2.311710d0.chunk.js"
+  },
+  {
+    "revision": "9062fc4a9ba6b57c9498",
+    "url": "/RoadTraffic/static/js/main.cd4ba7e9.chunk.js"
+  },
+  {
+    "revision": "e80cc0380f34ed27c5d0",
+    "url": "/RoadTraffic/static/js/runtime~main.b72c4805.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82cead78557033dd7d381da0584fe77e.js"
+  "/RoadTraffic/precache-manifest.40ae7afc0d71caa129ee4c2d7752be40.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RoadTraffic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
