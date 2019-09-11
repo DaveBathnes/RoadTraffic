2e125b46820d909b45922b9340bc904c2343e460
Dave <DaveBathnes@users.noreply.github.com>
2019-09-12 00:03:17 +0100

    Updates

---
+++ precache-manifest.82cead78557033dd7d381da0584fe77e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff94d73d3cc689ed0029a798d49cbfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a598e82dde2061aa3767",
+    "url": "/static/css/2.e71d8db5.chunk.css"
+  },
+  {
+    "revision": "a598e82dde2061aa3767",
+    "url": "/static/js/2.ebbdd532.chunk.js"
+  },
+  {
+    "revision": "4532c4589d036b46cd92",
+    "url": "/static/js/main.e4832dd8.chunk.js"
+  },
+  {
+    "revision": "50aaefef471a8cd4ec8e",
+    "url": "/static/js/runtime~main.1e1f8870.js"
+  }
+]);
